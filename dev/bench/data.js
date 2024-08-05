fa2505f1050806c0c87d1a1cf9fd25eae788c890
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 03:48:33 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 7e0079362a51cf740bb30b8d53270938dd01c782

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722825793517,
+  "lastUpdate": 1722829713376,
   "repoUrl": "https://github.com/JingZhe-Shan_fisglbl/qtm-benchmark",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 176128062.62626258,
             "unit": "ns",
             "range": "± 11464431.838558704"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jingzhe.shan@fisglobal.com",
+            "name": "Jing Zhe Shan",
+            "username": "JingZhe-Shan_fisglbl"
+          },
+          "committer": {
+            "email": "jingzhe.shan@fisglobal.com",
+            "name": "Jing Zhe Shan",
+            "username": "JingZhe-Shan_fisglbl"
+          },
+          "distinct": true,
+          "id": "7e0079362a51cf740bb30b8d53270938dd01c782",
+          "message": "update a test",
+          "timestamp": "2024-08-05T15:44:58+12:00",
+          "tree_id": "0163dc71c68cf8f15ff4e08bc702ce44fd177b1f",
+          "url": "https://github.com/JingZhe-Shan_fisglbl/qtm-benchmark/commit/7e0079362a51cf740bb30b8d53270938dd01c782"
+        },
+        "date": 1722829711922,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Quantum.Benchmark.Pipeline.FileIoTest.PipeLines",
+            "value": 76144288.82978724,
+            "unit": "ns",
+            "range": "± 5486302.7847959595"
+          },
+          {
+            "name": "Quantum.Benchmark.Pipeline.FileIoTest.AsyncStream",
+            "value": 168204889.58333334,
+            "unit": "ns",
+            "range": "± 12279564.233016787"
+          },
+          {
+            "name": "Quantum.Benchmark.Pipeline.FileIoTest.CsvHelper",
+            "value": 296445292.85714287,
+            "unit": "ns",
+            "range": "± 20492515.831744"
           }
         ]
       }
